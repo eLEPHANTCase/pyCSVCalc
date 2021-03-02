85b66f46ee178921abeb6e28273203481ba934f2
Orville Dillon <60524259+eLEPHANTCase@users.noreply.github.com>
2021-03-01 23:58:10 -0500

    Fix: Updated division.

--- Calculations/divide.py
+++ Calculations/divide.py
@@ -1,7 +1,10 @@
+def divisiondecor8r(method):
+    def check_division(a, b):
+        if b == 0:
+            print("Cannot divide by zero!")
+            return
+        return method(a, b)
+
 @divisiondecor8r
-def divide(a, b):
-    try:
-        a/b
-    except ZeroDivisionError:
-        print("Cannot divide by zero!")
-        return
+def division(a, b):
+    return float(a) / float(b
